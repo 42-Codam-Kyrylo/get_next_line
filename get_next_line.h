df1f8a1626465fef382c1b0d8094bf189a7cb7fa
Kyrylo Volynskyi <kvolynsk@f0r6s6.codam.nl>
2025-11-10 20:34:29 +0100

    tests added

---
+++ get_next_line.h
@@ -0,0 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   get_next_line.h                                    :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: kvolynsk <kvolynsk@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2025/11/10 15:23:21 by kvolynsk      #+#    #+#                 */
+/*   Updated: 2025/11/10 15:25:13 by kvolynsk      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+char	*get_next_line(int fd);
+
+#endif
