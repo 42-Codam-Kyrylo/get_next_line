e93f6ff6801c7828c8eee2b8021c19caaefbfdcd
Kyrylo Volynskyi <kvolynsk@f0r2s19.codam.nl>
2025-11-11 17:10:57 +0100

    started to work

--- get_next_line.c
+++ get_next_line.c
@@ -6,14 +6,24 @@
 /*   By: kvolynsk <kvolynsk@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/11/10 15:54:20 by kvolynsk      #+#    #+#                 */
-/*   Updated: 2025/11/10 20:33:35 by kvolynsk      ########   odam.nl         */
+/*   Updated: 2025/11/11 17:10:39 by kvolynsk      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+#include <stdlib.h>
+#include <unistd.h>
 
 char	*get_next_line(int fd)
 {
-	if (fd){}
-	return ("");
+	char *buffer = malloc(BUFFER_SIZE + 1);
+	int result = read(fd, buffer, BUFFER_SIZE);
+	if (result == -1)
+	{
+		return (NULL);
+	}
+
+	buffer[BUFFER_SIZE + 1] = '\0';
+
+	return (buffer);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,12 +6,16 @@
 /*   By: kvolynsk <kvolynsk@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/11/10 15:23:21 by kvolynsk      #+#    #+#                 */
-/*   Updated: 2025/11/10 15:25:13 by kvolynsk      ########   odam.nl         */
+/*   Updated: 2025/11/11 17:00:21 by kvolynsk      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10
+# endif
 
 char	*get_next_line(int fd);
 
